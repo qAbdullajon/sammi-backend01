13c346493bfcace2d0c7826da009cc1b9940e899
qAbdullajon <azikmelor7705@gmail.com>
2025-01-25 01:07:00 +0500

    db-url

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-require('dotenv').config()
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-const express = require('express')
-const mongoose = require('mongoose')
-const fileUpload = require('express-fileupload')
-const cookieParser = require('cookie-parser')
-const errorMiddleware = require('./middlewares/error.middleware')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const errorMiddleware = require("./middlewares/error.middleware");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
 app.use(
-	cors({
-		credentials: true,
-		origin: process.env.CLIENT_URL,
-	})
-)
-app.use(express.json())
-app.use(cookieParser({}))
-app.use(express.static('static'))
-app.use(fileUpload({}))
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+app.use(express.json());
+app.use(cookieParser({}));
+app.use(express.static("static"));
+app.use(fileUpload({}));
 
 // Routes
-app.use('/api/post', require('./routes/post.route'))
-app.use('/api/auth', require('./routes/auth.route'))
+app.use("/api/post", require("./routes/post.route"));
+app.use("/api/auth", require("./routes/auth.route"));
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 const bootstrap = async () => {
-	try {
-		await mongoose.connect(process.env.DB_URL).then(() => console.log('Connected DB'))
+  try {
+    await mongoose.connect(process.env.DB_URL).then(() => console.log("Connected DB"));
 
-		app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`))
-	} catch (error) {
-		console.log(`Error connecting with DB: ${error}`)
-	}
-}
+    app.listen(PORT, () => console.log(`Listening on - http://localhost:${PORT}`));
+  } catch (error) {
+    console.log(`Error connecting with DB: ${error}`);
+  }
+};
 
-bootstrap()
+bootstrap();
